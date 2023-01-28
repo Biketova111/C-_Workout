79f68c894841546f6cf5730e35c5db19c8897e5e
Biketova111 <89241781930@mail.ru>
2023-01-28 13:40:09 +0900

    Add task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Если целое число m делится нацело на целое число n, то вывести на экран частное от деления, 
+в противном случае вывести сообщение «m на n нацело не делится».*/
+
+Console.Write("Введите первое число, m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите первое число, n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m%n == 0)
+{
+    Console.WriteLine($"m/n = {m/n}");
+}
+else
+{
+    Console.WriteLine("m на n нацело не делится");
+    
+}
